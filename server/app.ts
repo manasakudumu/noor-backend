3b0ec14ffbf1c86feb43d6981e5d56cae1c0c39d
manasa <manasa@manasas-MacBook-Air.local>
2024-10-03 19:24:56 -0400

    a4

--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,7 @@
+import AlertingConcept from "./concepts/alerting";
 import AuthenticatingConcept from "./concepts/authenticating";
 import FriendingConcept from "./concepts/friending";
+import MonitoringConcept from "./concepts/monitoring";
 import PostingConcept from "./concepts/posting";
 import SessioningConcept from "./concepts/sessioning";
 
@@ -9,3 +11,5 @@ export const Sessioning = new SessioningConcept();
 export const Authing = new AuthenticatingConcept("users");
 export const Posting = new PostingConcept("posts");
 export const Friending = new FriendingConcept("friends");
+export const Alerting = new AlertingConcept("alerts");
+export const Monitoring = new MonitoringConcept("monitors");
